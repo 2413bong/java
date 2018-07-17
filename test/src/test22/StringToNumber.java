c27ea94943082f23efa02eb69efcdd9f7f5d2a48
kbg <kbg@DESKTOP-IDGE35M>
2018-07-17 15:25:54 +0900

    dad

--- test/src/test22/StringToNumber.java
+++ test/src/test22/StringToNumber.java
@@ -2,23 +2,33 @@ package test22;
 
 import java.util.Scanner;
 
-public class StringToNumber {
 
-	public static void main(String[] args) {
-		System.out.println("숫자 변환 프로그램을 시작합니다.");
-		Scanner scan = new Scanner(System.in);
-		System.out.println("숫자를 입력해 주세요");
-		int num=0;
-		boolean isOk = true;
-		while(isOk){
-			try {
+
+public class StringToNumber {
+	static Scanner scan = new Scanner(System.in);
+	static int num;
+	public static boolean parseInt() {
+		try {
+			System.out.println("숫자를 입력해 주세요");
 			num = Integer.parseInt(scan.nextLine());
-			isOk=false;  
+			return true;  
 		}catch(NumberFormatException nfe) {
 			System.out.println("입력하라는 숫자는 안입력하고!");
 		}
+		return false;
 		
-		}
+		
+	}
+
+	public static void main(String[] args) { 
+		System.out.println("숫자 변환 프로그램을 시작합니다.");
+		
+		
+		boolean isOk = false;
+		while(!isOk){
+			isOk=parseInt();
+			
+			}
 		
 		System.out.println("니가 입력한 숫자는 " + num +"이구나");
 		System.out.println("프로그램이 정상 종료되었습니다");
